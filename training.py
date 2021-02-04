f53a7af2fd423b7c24ec2115dcfda4c7762a2fb8
milescarberry <62004569+milescarberry@users.noreply.github.com>
2021-02-04 21:10:22 +0530

    my_second_commit

--- training.py
+++ training.py
@@ -8,6 +8,7 @@ import numpy as np
 
 import nltk
 
+
 from nltk.stem import WordNetLemmatizer
 
 
@@ -43,17 +44,74 @@ documents = []
 ignore_letters = ["?", "!", ",", ":", "."]
 
 
-for intent in data["intents"]:
+for intent in swiggy_data["intents"]:
 
 
     for pattern in intent["patterns"]:
 
         words_list = nltk.word_tokenize(pattern)
 
-        words.append(words_list)
+        words.extend(words_list)
 
         documents.append((words_list, intent["tag"]))
 
         if intent["tag"] not in classes:
 
             classes.append(intent["tag"])
+
+
+
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
+
+
+words = sorted(list(set(words)))
+
+classes = sorted(list(set(classes)))
+
+
+
+with open("data.pickle", "wb") as data_file:
+
+    pickle.dump((words,classes,documents), data_file)
+
+
+
+
+training = []
+
+output = []
+
+
+out_empty = [0 for _ in range(len(classes))]
+
+
+for document in documents:
+
+    bag = []
+
+
+    word_pattern = document[0]
+
+    word_pattern = [lemmatizer.lemmatize(word.lower()) for word in word_pattern if word not in ignore_letters]
+
+
+
+    for word in words:
+
+        if word in word_pattern:
+
+            bag.append(1)
+
+        else:
+
+            bag.append(0)
+
+
+    output_row = out_empty[::1]
+
+    output_row[classes.index(document[1])] = 1
+
+
+    training.append([bag, output_row])
+
+    
